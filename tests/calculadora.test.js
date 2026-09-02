e81ab9d8092b84092825faff017b5a894f115351
Thiago <thiagobelpo@gmail.com>
2026-09-01 21:38:08 -0300

    test: agregar pruebas adicionales para la función sumar

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -7,3 +7,15 @@ test("suma correctamente dos números", () => {
 
     expect(resultado).toBe(5);
 });
+
+test("2 + 3 devuelve 5", () => {
+  expect(sumar(2, 3)).toBe(5);
+});
+
+test("0 + 5 devuelve 5", () => {
+  expect(sumar(0, 5)).toBe(5);
+});
+
+test("-2 + 2 devuelve 0", () => {
+  expect(sumar(-2, 2)).toBe(0);
+});
